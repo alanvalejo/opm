3244d74fbd7a107a4a53314b081a264b317701ff
Alan Valejo <alanvalejo@gmail.com>
2018-02-01 13:09:52 -0200

    Update coarsening.py

--- coarsening.py
+++ coarsening.py
@@ -18,6 +18,11 @@ unlimited applications, distribute in binary or object form only, modify
 source-code and distribute modifications (derivative works). Please, giving 
 credit to the author by citing the papers. License will expire in 2018, July.
 
+Owner or contributors are not liable for any direct, indirect, incidental, special, 
+exemplary, or consequential damages, (such as loss of data or profits, and others)
+arising in any way out of the use of this software, even if advised of the possibility
+of such damage.
+
 Required:
 	.. _igraph: http://igraph.sourceforge.net
 	.. _numpy:  http://www.numpy.org
